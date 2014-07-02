22622f6065c4756c40a8d6b45308fff3768dae8e
Micah Eschbacher <michael-e@superbrightleds.com>
2014-07-02 12:30:35 -0500

    Can now create a basic creator to the tmp directory

--- src/MindOfMicah/Crudders/Commands/AddCreatorCommand.php
+++ src/MindOfMicah/Crudders/Commands/AddCreatorCommand.php
@@ -5,6 +5,10 @@ use Illuminate\Console\Command;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 
+use MindOfMicah\Classy\Filey;
+use MindOfMicah\Classy\Classy;
+use MindOfMicah\Classy\Funky;
+
 class AddCreatorCommand extends Command {
 
 	/**
@@ -41,7 +45,26 @@ class AddCreatorCommand extends Command {
 		//
         $this->info('Created');
 
-        
+        $filey=  new Filey;
+        $classy =new Classy('SampleCreator');
+        $classy->willExtend('MindOfMicah\Crudders\Creator'); 
+        $classy->willImplement('MindOfMicah\Crudders\Interfaces\CreationHandler');
+    
+          $f = new Funky('create');
+          $f->Param('array $inputs = []');
+          $f->Param('\MindOfMicah\Crudders\Interfaces\CreationHandler $callable = null');
+
+        $classy->addFunction($f);
+        $f = new Funky('onCreationSuccess');
+        $f->param('Eloquent $model');
+        $classy->addFunction($f);
+        $f = new Funky('onCreationError');
+        $f->param('Illuminate\Support\MessageBag $errors');
+        $classy->addFunction($f);
+        $filey->append($classy);
+
+
+        $contents = $filey->render() . "\n";;        
         file_put_contents('tests/tmp/SampleCreator.php', $contents);
 	}
 
--- tests/tmp/SampleCreator.php
+++ tests/tmp/SampleCreator.php
@@ -1,1 +1,18 @@
-hi
+<?php
+use MindOfMicah\Crudders\Creator;
+use MindOfMicah\Crudders\Interfaces\CreationHandler;
+use Eloquent;
+use Illuminate\Support\MessageBag;
+
+class SampleCreator extends Creator implements CreationHandler
+{
+    public function create(array $inputs = [], CreationHandler $callable = null)
+    {
+    }
+    public function onCreationSuccess(Eloquent $model)
+    {
+    }
+    public function onCreationError(MessageBag $errors)
+    {
+    }
+}
